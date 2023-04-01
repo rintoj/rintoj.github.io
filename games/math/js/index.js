53bc1d46b5393dbb00cce3b19ffad7073a7b6a01
Rinto Jose <rintoj@gmail.com>
2023-04-01 11:24:12 +0530

    fix: add game

---
+++ games/math/js/index.js
@@ -0,0 +1,69 @@
+
+const operations = ['+', '-', '*']
+
+function generateNumber() {
+  const multiplier = [10, 100]
+  const index = parseInt(`${((Math.random() * 10) / 2) % 2}`)
+  return parseInt(`${Math.random() * multiplier[index]}`)
+}
+
+function getOperator() {
+  const index = parseInt(`${((Math.random() % 0.33) * 10) % 3}`)
+  return operations[index]
+}
+
+function calculateAnswer(number1, operator, number2) {
+  switch (operator) {
+    case '+':
+      return number1 + number2
+    case '-':
+      return number1 - number2
+    case '*':
+      return number1 * number2
+  }
+}
+
+function prepareQuestion() {
+  const number1Div = document.querySelector('#number1')
+  const number2Div = document.querySelector('#number2')
+  const operatorDiv = document.querySelector('#operator')
+  const answerInput = document.querySelector('#answer')
+  const correctAnswerInput = document.querySelector('#correct-answer')
+
+  const [number2, number1] = [generateNumber(), generateNumber()].sort()
+  const operator = getOperator()
+  const correctAnswer = calculateAnswer(number1, operator, number2)
+
+  number1Div.innerHTML = number1
+  number2Div.innerHTML = number2
+  operatorDiv.innerHTML = operator
+  correctAnswerInput.value = correctAnswer
+  answerInput.value = ''
+  correctAnswerInput.type = 'hidden'
+}
+
+function setAnswerStatus(status) {
+  const answerInput = document.querySelector('#answer')
+  switch(status) {
+    case 'correct':
+      answerInput.classList.remove('incorrect')
+      answerInput.classList.add('correct')
+      break;
+    case 'incorrect':
+      answerInput.classList.remove('correct')
+      answerInput.classList.add('incorrect')
+      break;
+  }
+}
+
+function submit() {
+  const answerInput = document.querySelector('#answer')
+  const correctAnswerInput = document.querySelector('#correct-answer')
+  const answer = answerInput.value
+  const correctAnswer = correctAnswerInput.value
+  const isCorrect = answer === correctAnswer
+  setAnswerStatus(isCorrect ? 'correct' : 'incorrect')
+  correctAnswerInput.type = 'number'
+}
+
+prepareQuestion()
